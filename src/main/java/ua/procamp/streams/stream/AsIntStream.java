5493532901fcc7f38eaa9f278d096bd16de56ff0
kobzardanila@gmail.com <wiseNcrazyman13>
2019-02-05 02:08:07 +0200

    Implement default iterator

--- src/main/java/ua/procamp/streams/stream/AsIntStream.java
+++ src/main/java/ua/procamp/streams/stream/AsIntStream.java
@@ -2,16 +2,25 @@ package ua.procamp.streams.stream;
 
 import ua.procamp.streams.function.*;
 
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AsIntStream implements IntStream {
 
-
+    private int[] values;
+    private int workLength;
+    private IntIterator intIterator;
 
     private AsIntStream() {
         // To Do
     }
 
     public static IntStream of(int... values) {
-        return null;
+        AsIntStream asIntStream = new AsIntStream();
+        asIntStream.intIterator = asIntStream.new DefaultIntStreamIterator();
+        asIntStream.values = asIntStream.initArray(values);
+        asIntStream.workLength = values.length;
+        return asIntStream;
     }
 
     @Override
@@ -46,7 +55,9 @@ public class AsIntStream implements IntStream {
 
     @Override
     public void forEach(IntConsumer action) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        while (intIterator.moveToNext()) {
+            action.accept(intIterator.getCurrentInt());
+        }
     }
 
     @Override
@@ -66,7 +77,53 @@ public class AsIntStream implements IntStream {
 
     @Override
     public int[] toArray() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Arrays.copyOfRange(values, 0, workLength);
+    }
+
+    private void setIterator(IntIterator intIterator) {
+        this.intIterator = intIterator;
+    }
+
+    private class DefaultIntStreamIterator implements IntIterator {
+        private int currentInd = -1;
+        private boolean isFinished = true;
+
+        public boolean moveToNext() {
+            ++currentInd;
+            this.isFinished = currentInd >= values.length;
+            return !this.isFinished;
+        }
+
+        public int getCurrentInt() {
+            return values[currentInd];
+        }
+
+        public int getCurrentIndex() {
+            return currentInd;
+        }
+
+        public void setCurrentIndex(int currentIndex) {
+            this.currentInd = currentIndex;
+        }
+
+        public boolean isFinished() {
+            return this.isFinished;
+        }
+
+        @Override
+        public void add(int element) {
+            int currentIndex = this.getCurrentIndex();
+            if (currentIndex < values.length) {
+                workLength = values.length +1;
+                values = initArray(values);
+            }
+            values[workLength] = element;
+        }
+    }
+
+    private int[] initArray(int[] oldValues) {
+        int[] newArray = Arrays.copyOf(oldValues, (int) (oldValues.length * 1.5) + 1);
+        return newArray;
     }
 
 }
